aa1775d6adfa281d3b03a0ec40f05b9eec7d2856
Bogusz Jelinski <Bogusz.Jelinski@skatteetaten.no>
2025-12-31 18:11:49 +0100

    init

--- App.tsx
+++ App.tsx
@@ -5,19 +5,28 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  PermissionsAndroid, 
+  Platform, 
+  StatusBar, 
+  StyleSheet, useColorScheme, View, 
+  Text } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import Geolocation, {
+  GeoError,
+  GeoPosition,
+} from "@react-native-community/geolocation";
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle='dark-content' />
       <AppContent />
     </SafeAreaProvider>
   );
@@ -25,13 +34,55 @@ function App() {
 
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
+  const [speedKmh, setSpeedKmh] = useState<number>(0);
+
+  useEffect(() => {
+    let watchId: number | null = null;
+
+    const requestPermission = async (): Promise<void> => {
+      if (Platform.OS === "android") {
+        const permissions = [
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+            PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION,
+            PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS 
+        ];
+        await PermissionsAndroid.requestMultiple(permissions);
+      }
+
+      watchId = Geolocation.watchPosition(
+        (position: GeoPosition) => {
+          // speed is meters per second (may be null)
+          const speedMps: number = position.coords.speed ?? 0;
+          setSpeedKmh(speedMps * 3.6);
+        },
+        (error: GeoError) => {
+          console.warn("Geolocation error:", error);
+        },
+        {
+          enableHighAccuracy: true,
+          distanceFilter: 0,
+          interval: 1000,
+          fastestInterval: 500,
+        }
+      );
+    };
+
+    requestPermission();
+    StatusBar.setHidden(true, 'slide'); // setHidden(hidden, animation)
+    return () => {
+      StatusBar.setHidden(false, 'slide'); 
+      if (watchId !== null) {
+        Geolocation.clearWatch(watchId);
+      }
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+       <Text style={styles.mirroredText}>
+        {speedKmh.toFixed(0)}
+      </Text>
     </View>
   );
 }
@@ -39,6 +90,15 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+   mirroredText: {
+    fontSize: 500,
+    color: "white",
+    fontWeight: "bold",
+    transform: [{ scaleY: -1 }],
   },
 });
 
